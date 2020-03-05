2b56417d9a515fb53556108a4dcae8f27c06a7f2
koreanwglasses <koreanwglasses@gmail.com>
2020-03-05 11:40:50 -0500

    simple test cases

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,9 @@ import { trefoil } from "./core/planar-poly-knot";
 import { transform } from "./core/planar-poly-knot";
 import { PlanarSpringKnot } from "./layout/planar-spring-knot";
 import { drawKnot } from "./renderers/canvas/poly-knot-diagram";
-import { dot, translate, scale, Vector2 } from "./utils/lin";
-import { quadraticBSpline } from "./renderers/canvas/utils";
-import { arcsAtCrossing } from "./core/planar-knot";
-import { Arc, Knot } from "./core/defaults/planar-poly-knot";
+import { dot, translate, scale } from "./utils/lin";
+import { Knot } from "./core/planar-poly-knot";
+import { nextArc } from "./core/planar-knot";
 
 const knot = trefoil();
 const m = dot(translate([200, 200]), scale(100));
@@ -31,7 +30,7 @@ ctx.lineWidth = 5;
 ctx.lineCap = "round";
 
 springKnot.localOptimize();
-// drawKnot(ctx, springKnot);
+drawKnot(ctx, springKnot);
 
 // const interval = setInterval(() => {
 //   const energy = springKnot.energy();
@@ -50,33 +49,20 @@ springKnot.localOptimize();
 //   drawKnot(ctx, springKnot);
 // }, 100);
 
-const knot2 = springKnot as Knot;
-// const knot2 = trefoil();
-// transform(knot2, m);
+const nextArcFn = nextArc;
 
-// const crossing = knot2.crossings[0];
+const knot2 = Knot.fromPlanarPolyKnot(springKnot);
+knot2.uncross(knot2.crossings[0], "negative");
+knot2.reorient();
+knot2.uncross(knot2.crossings[0], "positive");
+knot2.reorient();
+// knot2.uncross(knot2.crossings[0], "positive");
+// knot2.reorient();
 
-// const [upperOut, upperIn, lowerOut, lowerIn] = arcsAtCrossing(knot2, crossing);
+const canvas2 = document.getElementById("test-canvas2") as HTMLCanvasElement;
+const ctx2 = canvas2.getContext("2d");
 
-// const arc1: Arc = {
-//   begin: lowerIn.begin,
-//   end: upperOut.end,
-//   path: [...lowerIn.path, crossing.location, ...upperOut.path]
-// };
-
-// const arc2: Arc = {
-//   begin: upperIn.begin,
-//   end: lowerOut.end,
-//   path: [...upperIn.path, crossing.location, ...lowerOut.path]
-// };
-
-// knot2.arcs.splice(knot2.arcs.indexOf(upperOut), 1);
-// knot2.arcs.splice(knot2.arcs.indexOf(upperIn), 1);
-// knot2.arcs.splice(knot2.arcs.indexOf(lowerOut), 1);
-// knot2.arcs.splice(knot2.arcs.indexOf(lowerIn), 1);
-
-// knot2.arcs.push(arc1, arc2);
-
-// knot2.crossings.splice(knot2.crossings.indexOf(crossing), 1);
-
-drawKnot(ctx, knot2);
+ctx.strokeStyle = "#000";
+ctx.lineWidth = 5;
+ctx.lineCap = "round";
+drawKnot(ctx2, knot2);
